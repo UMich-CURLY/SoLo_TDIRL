cad237f0c00ad84c325c64cd8c57b9222721d628
yifan-cloud <yifanxu421@gmail.com>
2022-05-21 20:06:51 -0400

    add irl code template

---
+++ script/fetch_irl.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+from collections import namedtuple
+
+
+import img_utils
+from mdp import gridworld
+from mdp import value_iteration
+from deep_maxent_irl import *
+from maxent_irl import *
+from utils import *
+from lp_irl import *
+
+class IRL_Agent():
+    def __init__(self ):
+        pass
+
+    def read_csv(self):
+        # Get the feature map and trajectory.
+
+        pass
+
+    def deep_irl(self):
+        # feed the feature maps and traj into network and train.
+        pass
+
+    def save_weight(self):
+        pass
+
+    def eval(self):
+        pass
